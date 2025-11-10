959d5cff1d9eb46c02ab57dd6a319649685d6d26
Satyam123456751 <chaudharysatyam263@gmail.com>
2025-11-10 19:19:33 +0530

    Fix: Comment out all missing component imports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,26 +2,25 @@
 import ProtectedRoute from "./auth/ProtectedRoute";
 import AdminProtectedRoute from "./auth/AdminProtectedRoute";
 import CartProtectedRoute from "./auth/CartProtectedRoute";
-import { LayoutContext } from "./layout";
-import { layoutState, layoutReducer } from "./layout/layoutContext";
+// import { LayoutContext } from "./layout";
+// import { layoutState, layoutReducer } from "./layout/layoutContext";
 import { isAdmin, isAuthenticate } from "./auth/fetchApi";
-import PageNotFound from "./layout/PageNotFound";
-import ProductDetails from "./productDetails";
-import ProductByCategory from "./home/ProductByCategory";
-import CheckoutPage from "./order/CheckoutPage";
+// import PageNotFound from "./layout/PageNotFound";
+// import ProductDetails from "./productDetails";
+// import ProductByCategory from "./home/ProductByCategory";
+// import CheckoutPage from "./order/CheckoutPage";
 
 export {
-  
   ProtectedRoute,
   AdminProtectedRoute,
   CartProtectedRoute,
-  LayoutContext,
-  layoutState,
-  layoutReducer,
+  // LayoutContext,
+  // layoutState,
+  // layoutReducer,
   isAdmin,
   isAuthenticate,
-  PageNotFound,
-  ProductDetails,
-  ProductByCategory,
-  CheckoutPage,
+  // PageNotFound,
+  // ProductDetails,
+  // ProductByCategory,
+  // CheckoutPage,
 };
